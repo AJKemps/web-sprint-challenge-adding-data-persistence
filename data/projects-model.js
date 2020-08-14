80ed749b88632e9282af7d9df2b42e24832a68cb
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-08-14 13:02:12 -0400

    add project is working

--- data/projects-model.js
+++ data/projects-model.js
@@ -1,9 +1,45 @@
 const db = require("../data/db-config.js");
 
 module.exports = {
-  find,
+  findProjects,
+  findByProjectId,
+  addProject,
+  addResource,
+  findResources,
+  addTask,
+  findTasks,
 };
 
-function find() {
+function findProjects() {
+  return db("projects");
+}
+
+function findByProjectId(id) {
+  return db("projects").where({ id });
+}
+
+function addProject(project) {
+  return db("projects")
+    .insert(project)
+    .then((response) => {
+      const id = response[0];
+
+      return findByProjectId(id);
+    });
+}
+
+function addResource() {
+  return db("projects");
+}
+
+function findResources() {
+  return db("projects");
+}
+
+function addTask() {
+  return db("projects");
+}
+
+function findTasks() {
   return db("projects");
 }
--- router/projects-router.js
+++ router/projects-router.js
@@ -3,13 +3,28 @@ const router = require("express").Router();
 const Projects = require("../data/projects-model");
 
 router.get("/", (req, res) => {
-  Projects.find({})
+  Projects.findProjects({})
     .then((response) => {
       res.status(200).json({ message: response });
     })
     .catch((error) => {
       res.status(500).json({
         message: "there was an issue while acccessing the projects database",
+        error: error.message,
+      });
+    });
+});
+
+router.post("/", (req, res) => {
+  let newProject = req.body;
+
+  Projects.addProject(newProject)
+    .then((response) => {
+      res.status(200).json({ message: response });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "there was an issue while adding a project to the database",
         error: error.message,
       });
     });
